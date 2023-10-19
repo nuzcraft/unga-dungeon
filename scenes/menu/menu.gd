44fbe0a5a22e870a0c9ed7efa0ce0bce44b4f313
nuzcraft <nuzcraft@gmail.com>
2023-10-19 12:48:05 -0500

    added more tiles to gridmap

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -9,17 +9,7 @@ func _ready():
 
 
 func _on_PlayButton_pressed() -> void:
-	var params = {
-		"show_progress_bar": true,
-		"a_number": 10,
-		"a_string": "Ciao!",
-		"an_array": [1, 2, 3, 4],
-		"a_dict": {
-			"name": "test",
-			"val": 15
-		},
-	}
-	Game.change_scene_to_file("res://scenes/gameplay/gameplay.tscn", params)
+	Game.change_scene_to_file("res://scenes/gameplay/gameplay.tscn")
 
 
 func _on_ExitButton_pressed() -> void:
